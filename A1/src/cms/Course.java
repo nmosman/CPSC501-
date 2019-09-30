30b370c217672d77283000b35dda77813541eed3
Nasir Osman <nmosman@ucalgary.ca>
2019-09-30 15:17:53 -0600

    Add files via upload

---
+++ A1/src/cms/Course.java
@@ -0,0 +1,24 @@
+package cms;
+
+import java.util.*;
+
+public class Course {
+
+		private String name;
+		private String course_id;
+		private String instructor;
+		
+		private Date startDate;
+		private Date endDate; 
+		
+		private int class_size_max;
+		
+		private ArrayList<Student> studentList = new ArrayList<Student>();
+		
+		
+		
+		//setters 
+		//getters
+		
+		
+}
