1256552cd106f3cda675f2c621fb0a323acc3cef
Nasir Osman <nmosman@ucalgary.ca>
2019-09-30 16:27:18 -0600

    Update Student.java

--- A1/src/cms/Student.java
+++ A1/src/cms/Student.java
@@ -1,5 +1,15 @@
 package cms;
 
 public class Student {
-
+	private String name;
+	private String studentId;
+	
+	private StudentSchedule schedule;
+	
+	
+	public void joinCourse(String joinCode)
+	{
+		
+	}
+	
 }
