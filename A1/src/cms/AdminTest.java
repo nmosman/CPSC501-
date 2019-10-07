bfb532b301c3fce50015ab90fa5fa5bcc7bcb642
Nasir Osman <nmosman@ucalgary.ca>
2019-10-07 15:39:45 -0600

    Create AdminTest.java

---
+++ A1/src/cms/AdminTest.java
@@ -0,0 +1,14 @@
+package cms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdminTest {
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
